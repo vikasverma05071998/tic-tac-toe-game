80e2de8e5a03db9177f0917c56229292fef8345a
vikasverma05071998 <107770586+vikasverma05071998@users.noreply.github.com>
2022-10-30 16:07:31 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Board from './TicComponent/Board'
+import './TicComponent/tip.css'
+export default function App() {
+  return (
+    <div>
+   <h1>tic tac toe game</h1>  
+    <Board/>
+    </div>
+  )
+}
